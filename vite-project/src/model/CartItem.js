306eb068d685ec07e6494766bb99b940170e7927
VeshonaS <aniahshell1@gmail.com>
2024-07-10 15:37:04 -0400

    a bit confused

--- vite-project/src/model/CartItem.js
+++ vite-project/src/model/CartItem.js
@@ -1,7 +1,9 @@
 import getId from "../utils/getId";
 
 class CartItem {
-
+ id = getId()
+ name = this.name 
+ price = this.price 
 }
 
 export default CartItem;
--- vite-project/src/model/ShoppingCart.js
+++ vite-project/src/model/ShoppingCart.js
@@ -2,7 +2,19 @@ import getId from "../utils/getId";
 import CartItem from "./CartItem";
 
 class ShoppingCart {
+  id = getId()
+ static #cartItems = []
+  static ShoppingCart 
 
+  createItem( name , price ){
+    this.name = name 
+    this.price = price 
+    ShoppingCart.#cartItems.push(this)
+  }
+  getItems(){
+  
+  }
+ 
 }
 
 export default ShoppingCart;
